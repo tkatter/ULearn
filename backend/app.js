ee7b56be5c02702c3a31cffe637d7ffbb4dbd952
tkatter <tjkatter@gmail.com>
2025-04-16 00:14:19 -0500

    dealing with swagger css

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-// const swaggerUi = require('swagger-ui-express');
-// const YAML = require('yaml');
-// const fs = require('fs');
+const swaggerUi = require('swagger-ui-express');
+const options = require('./utils/customSwaggerCss');
+const YAML = require('yaml');
+const fs = require('fs');
 
 const setRouter = require('./routes/setRoutes');
 const noteRouter = require('./routes/noteRoutes');
 
 // SWAGGER API Documentation
-// const swaggerFile = fs.readFileSync('backend/api/index.yaml', 'utf-8');
-// const swaggerDocument = YAML.parse(swaggerFile);
+const swaggerFile = fs.readFileSync('backend/api/swagger.yaml', 'utf-8');
+const swaggerDocument = YAML.parse(swaggerFile);
 
 const app = express();
 
@@ -19,14 +20,31 @@ app.use(cors());
 
 // Middleware to parse body data from req.body
 app.use(express.json({ limit: '10kb' }));
-
+app.use(express.static(__dirname));
 // Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 // Routes
-// app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// const options = {
+//   customCss: `.swagger-ui .topbar {
+//     display: none;
+//   }
+//   .swagger-ui {
+//     background-color: #333;
+//   }`,
+// };
+
+// const options = {
+//   customCssUrl: '/customSwagger.css',
+// };
+
+app.use(
+  '/api/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, options)
+);
 
 app.use('/api/v1/sets', setRouter);
 app.use('/api/v1/notes', noteRouter);
---
+++ backend/utils/customSwaggerCss.js
@@ -0,0 +1,32 @@
+const options = {
+  customCss: `
+  html { 
+  font-size: 62.5%;
+  }
+  
+  .swagger-ui section.models,
+  .swagger-ui .info {
+    margin: 0;
+  }
+
+  .swagger-ui *,
+  .swagger-ui .info .title,
+  .swagger-ui .info p,
+  .swagger-ui .opblock .opblock-summary-description {
+    color: #fff;
+  }
+  .swagger-ui .topbar {
+    display: none;
+  }
+  .swagger-ui .scheme-container,  
+  .swagger-ui.swagger-container,
+  .swagger-ui * {
+    background-color: #333;
+  }
+  .swagger-ui{
+    padding: 2.8rem 0;
+  }
+  `,
+};
+
+module.exports = options;
